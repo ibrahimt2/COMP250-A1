a069ec24c3b71e7d4f03ecdec0340cea359a3670
Ibrahim T Miraj <zerobottles@outlook.com>
2019-05-09 15:31:06 -0400

    Completed the Fruit class

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,4 +1,32 @@
 
-public class Fruit {
+public class Fruit extends MarketProduct {
 
+	double weight;			//Weight in kilograms
+	int pricePerKilogram;	//Price in cents
+	
+	public Fruit(String name, double weight, int pricePerKilogram) {
+		super(name);
+		this.weight = weight;
+		this.pricePerKilogram = pricePerKilogram;
+	}
+	
+	public int getCost() {
+		return (int) (weight * pricePerKilogram);
+	}
+	
+	public boolean equals(Object object) {
+		if (!(object instanceof Fruit)) {
+			return false;
+		}
+		
+		if (this.getName() != ((MarketProduct) object).getName()) {
+			return false;
+		}
+		
+		if (this.getCost() != ((MarketProduct) object).getCost()) {
+			return false;
+		}
+		
+		return true;
+	}
 }
