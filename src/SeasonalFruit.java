a683d4f11355e384d5a92c1d6fd4d46e878050d2
Ibrahim T Miraj <zerobottles@outlook.com>
2019-05-09 15:57:32 -0400

    Added and wrote SeasonalFruit class

---
+++ src/SeasonalFruit.java
@@ -0,0 +1,11 @@
+public class SeasonalFruit extends Fruit {
+
+	public SeasonalFruit(String name, double weight, int pricePerKilo) {
+		super(name, weight, pricePerKilo);
+	}
+	
+	public int getCost() {
+		return (int) (super.getCost() * 0.85);
+	}
+	
+}
