8a1ca082ae53b7ab498b026436a943a15a89096d
Ibrahim T Miraj <zerobottles@outlook.com>
2019-05-09 15:49:12 -0400

    Added and wrote Jam class

---
+++ src/Jam.java
@@ -0,0 +1,32 @@
+public class Jam extends MarketProduct {
+
+	private int numberOfJars;
+	private int pricePerJar;
+	
+	public Jam(String name, int numberOfJars, int pricePerJar) {
+		super(name);
+		this.numberOfJars = numberOfJars;
+		this.pricePerJar = pricePerJar;
+	}
+	
+	public int getCost() {
+		return numberOfJars * pricePerJar;
+	}
+	
+	public boolean equals(Object object) {
+		if (!(object instanceof Jam)) {
+			return false;
+		}
+		
+		if (this.getName() != ((MarketProduct) object).getName()) {
+			return false;
+		}
+		
+		if (this.getCost() != ((MarketProduct) object).getCost()) {
+			return false;
+		}
+		
+		return true;
+	}
+}
+
